04978479d177a0dae5875ecfe7b7a55ef7af3ba0
CharaD7 <jijakahn6@gmail.com>
2022-08-20 13:08:44 +0000

    Top-right positioning

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'flex-end',
     // justifyContent: 'flex-start',
   },
 });
