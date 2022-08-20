cc868d035ee54daed24461931a49b4065f6c6e15
CharaD7 <jijakahn6@gmail.com>
2022-08-20 16:33:08 +0000

    Created Signup Component and Installed kaloraat text

--- App.js
+++ App.js
@@ -1,27 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import Welcome from './components/Welcome';
+import Signup from './screens/Signup';
 
 export default function App() {
 
-  const names = ['Ryan', 'Zen', 'David'];
 
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-
-      { names.map((name) => (
-        <Welcome key={name} name={name} />
-      )) }
-    </View>
+    <Signup />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import Text from '@kaloraat/react-native-text';
+
+const Signup = () => {
+  return (
+    <View style={{ flex:1, justifyContent: 'center' }}>
+      <Text title center>Signup</Text>
+    </View>
+  );
+};
+
+export default Signup;
