1756b042fbe04c7edc23da41ebec430efb1c0835
CharaD7 <jijakahn6@gmail.com>
2022-08-20 16:45:02 +0000

    Added styled TextInput

--- screens/Signup.js
+++ screens/Signup.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, TextInput } from 'react-native';
 import Text from '@kaloraat/react-native-text';
 
 const Signup = () => {
   return (
     <View style={{ flex:1, justifyContent: 'center' }}>
       <Text title center>Signup</Text>
+      <View style={{ marginHorizontal: 24 }}>
+        <Text semi>Name</Text>
+        <TextInput style={{
+          borderBottomWidth: 0.5,
+          height: 48,
+          borderBottomColor: '#8e93a1',
+          marginBottom: 30,
+        }} />
+      </View>
     </View>
   );
 };
