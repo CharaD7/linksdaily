c1c8a2f4346adb671526a7d8030cec623181661d
CharaD7 <jijakahn6@gmail.com>
2022-08-21 02:54:47 +0000

    Added reusable button

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import Signup from './screens/Signup';
 
 export default function App() {
 
-
   return (
     <Signup />
   );
--- screens/Signup.js
+++ screens/Signup.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
 import Text from '@kaloraat/react-native-text';
 import UserInput from '../components/auth/UserInput';
+import SubmitButton from '../components/auth/SubmitButton';
 
 const Signup = () => {
 
@@ -16,6 +17,8 @@ const Signup = () => {
       <UserInput name='NAME' value={name} setValue={setName} autoCapitalize='words' autoCorrect={false} />
       <UserInput name='EMAIL' value={email} setValue={setEmail} autoCompleteType='email' keyboardType='email-address' />
       <UserInput name='PASSWORD' value={password} setValue={setPassword} secureTextEntry={true} autoCompleteType='password' />
+
+      <SubmitButton title="Sign Up"/>
 
       <Text>{JSON.stringify({ name, email, password }, null, 4)}</Text>
     </View>
